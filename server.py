ab75e36754917c0e9c62a8588ffb6c5bd438d520
darko <ki14.skakun@gmail.com>
2023-07-08 18:10:44 +0300

    implemented binary search tree, and rout for get blog post by id

--- server.py
+++ server.py
@@ -5,7 +5,9 @@ from sqlalchemy.engine import Engine
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import linked_list
+import binary_search_tree
 import hash_table
+import random
 
 # app
 app = Flask(__name__)
@@ -145,14 +147,30 @@ def create_blog_post(user_id):
     return jsonify({"message": f"{ht.get_value('title')} - blog post is created."}), 200
 
 
-@app.route("/blog_post/<user_id>", methods=["GET"])
-def get_all_blog_posts(user_id):
-    pass
+# @app.route("/blog_post/<user_id>", methods=["GET"])
+# def get_all_blog_posts(user_id):
+#     pass
 
 
 @app.route("/blog_post/<blog_post_id>", methods=["GET"])
 def get_one_blog_post(blog_post_id):
-    pass
+    blog_posts = BlogPost.query.all()
+    random.shuffle(blog_posts)
+
+    bst = binary_search_tree.BinarySearchTree()
+    for post in blog_posts:
+        bst.insert({
+            "id": post.id,
+            "title": post.title,
+            "body": post.body,
+            "date": post.date,
+            "user_id": post.user_id
+        })
+    post = bst.search(blog_post_id)
+
+    if not post:
+        return jsonify({"message": "post not found"})
+    return jsonify(post)
 
 
 @app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
